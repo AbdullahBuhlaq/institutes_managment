395432cc877081fd32c9e6c5b8828a8fae17d1ef
Abdullah Buhlaq <3bdullah.bu7la8@gmail.com>
2023-10-22 17:12:58 +0300

    for host

--- desc.js
+++
@@ -1,9 +0,0 @@
-const { app, BrowserWindow } = require("electron");
-
-function createWindow() {
-  win = new BrowserWindow({ width: 800, height: 600, icon: "" });
-
-  win.loadFile("./public/index.html");
-}
-
-app.on("ready", createWindow);
